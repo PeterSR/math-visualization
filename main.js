bb98920c443acc75375d8bd5deaa2a4cd11c26ef
Peter Severin Rasmussen <git@petersr.com>
2022-03-08 08:43:34 +0100

    Experimented with during render in parallel

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-var zoom = 3
-var p1 = 1
-var p2 = 10
+
+
+
+let cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 }
+let cameraZoom = 1
+let MAX_ZOOM = 5
+let MIN_ZOOM = 0.1
+let SCROLL_SENSITIVITY = 0.0005
 
 function d(x,y) {
     return Math.sqrt(x*x + y*y)
 }
 
 function f(x, y) {
+    var p1 = 1
+    var p2 = 10
     return 2/(1 + Math.exp(-p1*(d(x,y)-p2))) -1
 }
 
 function eq1(x, y) {
+    var zoom = 3
     return Math.round(Math.cos(zoom*(x+y)) + Math.cos(zoom*(x-y)) + f(x, y)) < 0
 }
 
@@ -27,6 +35,68 @@ function render() {
 
     var shortestDim = Math.min(ctx.canvas.width, ctx.canvas.height)
 
+    var p = new Parallel(computeSections(ctx.canvas.width, ctx.canvas.height, 16, 12), {
+        env: {
+            ctx: ctx,
+            drawPixel: drawPixel,
+            remap, remap,
+            xMin: xMin,
+            xMax: xMax,
+            xPixelOffset: xPixelOffset,
+            yMin: yMin,
+            yMax: yMax,
+            yPixelOffset: yPixelOffset,
+            shortestDim: shortestDim,
+        },
+        envNamespace: 'parallel'
+    })
+
+    function renderSection(section) {
+        const {
+            xMin,
+            xMax,
+            xPixelOffset,
+            yMin,
+            yMax,
+            yPixelOffset,
+            shortestDim,
+        } = global.parallel;
+
+        pixels = []
+        for (let xPixel = section.xMin; xPixel < section.xMax; xPixel++) {
+            for (let yPixel = section.yMin; yPixel < section.yMax; yPixel++) {
+                const x = remap(xPixel-xPixelOffset, 0, shortestDim, xMin, xMax)
+                const y = remap(yPixel+yPixelOffset, 0, shortestDim, yMin, yMax)
+                if (eq1(x, y)) {
+                    pixels.push([xPixel, yPixel, 0, 0, 0])
+                } else {
+                    pixels.push([xPixel, yPixel, 255, 255, 255])
+                }
+            }
+        }
+        return pixels
+    };
+    function drawSection(results) {
+        results.forEach((pixels) => {
+            pixels.forEach((pixel) => {
+                drawPixel(ctx, pixel[0], pixel[1], pixel[2], pixel[3], pixel[4])
+            })
+        })
+        console.timeEnd("parallel")
+    }
+    p.require(getMainCanvas)
+    p.require(drawPixel)
+    p.require(remap)
+    p.require(eq1)
+    p.require(f)
+    p.require(d)
+
+    console.time("parallel")
+    p.map(renderSection).then(drawSection)
+
+    console.time("traditional")
+
+        /*
     for (var xPixel = 0; xPixel < ctx.canvas.width; xPixel++) {
         for (var yPixel = 0; yPixel < ctx.canvas.height; yPixel++) {
             var x = remap(xPixel-xPixelOffset, 0, shortestDim, xMin, xMax)
@@ -39,4 +109,8 @@ function render() {
             }
         }
     }
+    */
+
+    console.timeEnd("traditional")
+
 }
--- util.js
+++ util.js
@@ -20,6 +20,34 @@ function almostEqual(v1, v2, epsilon) {
     return Math.abs(v1 - v2) < epsilon
 }
 
+function computeSections(width, height, xChunks, yChunks) {
+    if (xChunks == null) {
+        xChunks = 1
+    }
+    if (yChunks == null) {
+        yChunks = 1
+    }
+
+    const sections = []
+
+    const xJump = Math.floor(width / xChunks)
+    const yJump = Math.floor(height / yChunks)
+
+    for (var xPixel = 0; xPixel < width; xPixel += xJump) {
+        for (var yPixel = 0; yPixel < height; yPixel += yJump) {
+            section = {
+                xMin: xPixel,
+                xMax: Math.min(xPixel + xJump, width),
+                yMin: yPixel,
+                yMax: Math.min(yPixel + yJump, height),
+            }
+            sections.push(section)
+        }
+    }
+
+    return sections
+}
+
 function onResize() {
     var ctx = getMainCanvas()
     ctx.canvas.width  = window.innerWidth;
